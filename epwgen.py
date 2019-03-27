1bf2cfd2cc7e8f75eb8fd0f4a419718067e2147e
noemas <48996113+noemas@users.noreply.github.com>
2019-03-27 17:53:21 +0000

    Update epwgen.py

--- epwgen.py
+++ epwgen.py
@@ -1,29 +1,3 @@
-#Use this script to generate all necessary input files to calculate the electron phonon coupling of a system using the
-#EPW package of Quantum Espresso. 
-#The calculations need to be run on a cluster with IBM® Platform™ LSF® implementation however modifying it
-#to run on different workload schedulers like SLURM should not be difficult (positions of necessary changes are denoted by "LSF" in this script).
-
-#The script will generate a parent directory called $base_dir which itself contains the following directories:
-# - ELB (directory for electron band calculations)
-# - PHB (phonon bands)
-# - EPM (electron phonon matrix, phonon linewidhts and Eliashberg spectral function)
-# - ISO (solving the isotropic Eliashberg equations)
-
-#All you need to do is to specify the calculation parameters below and then run the submission scripts called
-#ep_bands.sh and epw.sh on the cluster in that order.
-#ep_bands.sh will prepare and submit the calculations for the electron and phonon bands. Because image parallelization
-#does not work for the needed type of phonon calculations, this script splits the phonon calculation into multiple calculations;
-#one calculation for each irreducible q-point and additionally one calculation for every irreducible representation of
-#each irreducible q-point. This splitting can also be disabled.
-
-#Once the electron and phonon band calculations are finished, check the results and then execute epw.sh.
-#This will update the EPW input files with the data from the electron and phonon bands calculations and then submit them.
-
-#Check the submission scripts for further options/instructions.
-
-#Any calculation parameters that are missing or have been "hardcoded" should be easy enough
-#to introduce/change in the input lists called *_in. Everything should still work if you wish to do so.
-
 #______________________RUN_ENVIRONMENT_______________________#
 #prefix for any data files
 pf = 'Pm-3m_q8_e0_bands'  
